a8e733ebb9f770a5e5be04dd9f89e5cfb5095142
Ivan <feb>
2017-08-11 21:26:39 +0500

    StringFormatter refactoring

--- src/StringFormatter.java
+++ src/StringFormatter.java
@@ -5,26 +5,20 @@ public class StringFormatter {
   public static void main (String[] args){
 
     String inputString = "сапог саше сарай арбуз аб болт бокс биржа";
-    List<String> wordsList = new ArrayList<>(Arrays.asList(inputString.split(" ")));
-    StringBuilder sb = new StringBuilder("[");
-    HashMap<String, List<String>> wordsMap = new HashMap<>();
-    for(String item : wordsList){
-      String s = String.valueOf(item.charAt(0));
-      if(!wordsMap.containsKey(s)){
+    SortedSet<String> wordsSet = new TreeSet<>(Arrays.asList(inputString.split(" ")));
+    TreeMap<String, List<String>> wordsMap = new TreeMap<>();
+    for(String item : wordsSet){
+      String s = item.substring(0,1);
+      if(!wordsMap.containsKey(item.substring(0,1))){
         List<String> l = new ArrayList<>();
         l.add(item);
         wordsMap.put(s, l);
       }else{
         wordsMap.get(s).add(item);
-        wordsMap.get(s).sort(new StringComparator());
       }
     }
-    List<String> resultList = new ArrayList<>();
-    for(String key : wordsMap.keySet()){
-      resultList.add(key +"=["+String.join(",", wordsMap.get(key))+"]");
-    }
-    sb.append(String.join(", ", resultList)).append("]");
-    System.out.println(sb.toString());
+    wordsMap.forEach((k, v) -> v.sort(new StringComparator()));
+    System.out.println(wordsMap.toString().replaceAll("\\{", "[").replaceAll("\\}","]"));
   }
 }
 
